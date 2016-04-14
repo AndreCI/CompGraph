6098f37b0f7b24b2f3603eae2d342d6ae03aaab8
AndreCI <andre.cibils@gmail.com>
2016-04-14 16:20:30 +0200

    Preparing 1.2.3
    Not working!

--- projet-Janjar-Cibils-Premi/src/grid/grid_vshader.glsl
+++ projet-Janjar-Cibils-Premi/src/grid/grid_vshader.glsl
@@ -11,10 +11,9 @@ uniform float time;
 void main() {
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
-    // convert the 2D position into 3D positions that all lay in a horizontal
-    // plane.
-    // TODO 6: animate the height of the grid points as a sine function of the
-    // 'time' and the position ('uv') within the grid.
+    /* TODO 1.2 : 2 Create a shader program for terrain rendering.
+The vertex shader samples the height map texture an displaces the vertices according to the height.
+(reuse/adapt from HW3)*/
     float height = (texture(tex,uv).r+texture(tex,uv).b+texture(tex,uv).g)/3;
     vec3 pos_3d = vec3(position.x, height, -position.y);
     gl_Position = MVP * vec4(pos_3d, 1.0);
