543c2d164aad070dbfd7160e0cfde4ba56220de9
AndreCI <andre.cibils@gmail.com>
2016-04-14 16:48:36 +0200

    1.2.3 done
    (I hope)

--- projet-Janjar-Cibils-Premi/src/grid/grid_fshader.glsl
+++ projet-Janjar-Cibils-Premi/src/grid/grid_fshader.glsl
@@ -1,34 +1,28 @@
 #version 330
 
 in vec2 uv;
+
 out vec3 color;
 out float height;
 uniform sampler2D tex;
 
-
-in vec3 light_dir;
-uniform vec3 kd,Ld;
-
-
-
 void main() {
-
+    color = vec3(0.0,0.0,0.0);
     /*TODO 1.2 : 3 Diffuse shading of the terrain in the fragment shader.
-You have to calculate the normal at every position in the fragment shader.
-One way to do this is by using finite differences: you can find the gradient at the current pixel position by
-comparing the elevation with the elevation of the neighboring pixels in the height map*/
-
-
-    vec3 X = dFdx(vec3(uv.x,uv.y,height));
-    vec3 Y = dFdy(vec3(uv.x,uv.y,height));
-    vec3 normal = (cross(X,Y));
-    vec3 n = normalize(normal);
-
-    vec3 l = normalize(light_dir);
-    vec3 Id = kd * dot(n,l) * Ld;
+You have calculate the normal at every position in the fragment shader.
+One way to do this is by using finite differences: you can find the gradient
+at the current pixel position bycomparing the elevation with the elevation of
+the neighboring pixels in the height map*/
     //kd = couleur du matériel
     //Ld = couleur du soleil
-    //light_dir = a nous de choisir ou est le soleil
-   // color = texture(tex, uv).rgb;
-    color = Id;
+    vec3 Y = dFdy(vec3(uv,height));
+    vec3 X = dFdx(vec3(uv,height));
+    vec3 normal = normalize(cross(X,Y));
+
+    vec3 kd = normalize(vec3(0.8,0.8,0.8));
+    vec3 Ld = normalize(vec3(0.8,0.8,0.8));
+
+    color = kd*normal*Ld;
+
+
 }
