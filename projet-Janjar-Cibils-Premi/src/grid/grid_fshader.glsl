e90d332824ad28061647c5ae781db7eb20e25862
AndreCI <andre.cibils@gmail.com>
2016-04-15 14:18:11 +0200

    Starting 1.2.4

--- projet-Janjar-Cibils-Premi/src/grid/grid_fshader.glsl
+++ projet-Janjar-Cibils-Premi/src/grid/grid_fshader.glsl
@@ -3,7 +3,7 @@
 in vec2 uv;
 
 out vec3 color;
-out float height;
+in float height;
 
 uniform sampler2D colorTex;
 uniform sampler2D tex;
@@ -16,17 +16,27 @@ You have calculate the normal at every position in the fragment shader.
 One way to do this is by using finite differences: you can find the gradient
 at the current pixel position bycomparing the elevation with the elevation of
 the neighboring pixels in the height map*/
-    vec3 Y = dFdy(vec3(uv.x,height,uv.y));
-    vec3 X = dFdx(vec3(uv.y,uv.y,height));
-    vec3 normal = normalize(cross(X,Y));
+    vec3 pos = (vec3(uv.x,uv.y,height));
+    vec3 Y = normalize(dFdy(pos));
+    vec3 X = normalize(dFdx(pos));
+    vec3 normal = (cross(X,Y));
     //normal = normal a un point
-    vec3 light_dir = normalize(vec3(0,0,10));
+    vec3 light_dir = normalize(vec3(10,10,10));
     //light_dir = direction de la lumiere
-    vec3 kd = vec3(1,0.8,0.8);//texture(tex,uv).rgb;
+    vec3 kd = vec3(1,1,1);//texture(tex,uv).rgb;
+   if(height>0.8){
+        kd = vec3(255-height*(255-229),255,153+height*(204-153));
+    }else if(height>0.3){
+        kd=vec3(255-(height-0.3)/0.5*(255-204),255,153);
+    }else{
+        kd=vec3(255,255,153);
+    }
+    kd = normalize(kd);
+   // kd = vec3(height,height,1-height);
+
     //kd = couleur du matériel
-    vec3 Ld = normalize(vec3(1,0.8,1));
+    vec3 Ld = normalize(vec3(1,1,1));
     //Ld = couleur du soleil
 
     color = kd * dot(normal,light_dir) * Ld;
-
 }
--- projet-Janjar-Cibils-Premi/src/grid/grid_vshader.glsl
+++ projet-Janjar-Cibils-Premi/src/grid/grid_vshader.glsl
@@ -3,6 +3,7 @@
 in vec2 position;
 
 out vec2 uv;
+out float height;
 
 uniform sampler2D colorTex;
 uniform mat4 MVP;
@@ -14,7 +15,7 @@ void main() {
     /* TODO 1.2 : 2 Create a shader program for terrain rendering.
 The vertex shader samples the height map texture an displaces the vertices according to the height.
 (reuse/adapt from HW3)*/
-    float height = (texture(colorTex,uv).x + texture(colorTex,uv).y)/2;
+    height = (texture(colorTex,uv).x + texture(colorTex,uv).y);
     vec3 pos_3d = vec3(position.x, height, -position.y);
     gl_Position = MVP * vec4(pos_3d, 1.0);
 }
