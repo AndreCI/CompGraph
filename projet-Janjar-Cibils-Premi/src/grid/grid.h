b05cb85f1974a86f78a5659c889cbbfb36a35e72
AndreCI <andre.cibils@gmail.com>
2016-05-31 10:08:30 +0200

    better small river, and some new water texture (nned to chose one)

--- projet-Janjar-Cibils-Premi/src/grid/grid.h
+++ projet-Janjar-Cibils-Premi/src/grid/grid.h
@@ -230,7 +230,7 @@ class Grid {
 
                 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-                filename ="water_4.jpg";
+                filename ="water_1.jpg";
                 stbi_set_flip_vertically_on_load(1);
                 unsigned char* image_water = stbi_load(filename.c_str(), &width,
                                                  &height, &nb_component, 0);
